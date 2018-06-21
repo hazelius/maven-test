c09c99e6a0f13257105b8e3569bfb127431c0bf9
Yosuke Hasegawa <yousuke.hasegawa@cydas.com>
2018-06-21 15:52:11 +0900

    package名修正

--- src/test/java/myscenarios/MyscenariosTest.java
+++ src/test/java/myscenarios/MyscenariosTest.java
@@ -1,10 +1,10 @@
-package postnumbers;
+package myscenarios;
 
 import com.intuit.karate.junit4.Karate;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
-public class PostnumbersTest {
+public class MyscenariosTest {
     // this will run all *.feature files that exist in sub-directories
     // refer to https://github.com/intuit/karate#naming-conventions
     // for folder-structure recommendations and naming conventions
